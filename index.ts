22c77d1e39f6dcb9a706fc355f63b5844659a605
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2023-09-15 09:29:00 +1000

    initial commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+import { BunFile } from "bun";
+
+const args: string[] = Bun.argv.slice(2);
+let setArgs: string[] = [];
+let pathString: string = "";
+
+for (let i = 0; i < args.length; i++) {
+  const arg = args[i];
+
+  if (!arg) {
+    console.log(`Argument: ${i} is invalid`);
+  }
+
+  if (arg.startsWith("-set=")) {
+    let number = arg.indexOf("-set=") + "-set=".length;
+    const setString = arg.slice(number)
+    setArgs = setString.split(',');
+  }
+
+  if (arg.startsWith("-path=")) {
+    let number: number = arg.indexOf("-path=") + "-path=".length;
+    pathString = arg.slice(number);
+  }
+}
+
+const file: BunFile = Bun.file(pathString);
+
+const contents = await file.json();
+
+let arr: any[] = [];
+
+for (let index = 0; index < contents.length; index++) {
+  const element = contents[index];
+
+  if (setArgs.includes(element.set)) {
+    arr.push(element);
+  }
+  
+}
+
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+}
+
+console.log(`number of cards in sets: ${setArgs.join(',')} is: ${arr.length}`);
+
+console.log(`Took: ${Bun.nanoseconds()/1000000000}seconds`);
+
